48a5ada3217e185223fe80689b889bd3d6bf449e
Tao-Nhan Nguyen <tao.nhan@gmail.com>
2021-07-02 11:20:02 -0400

    feat : some feature

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/taonhannguyen/github-actions-react-app",
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github'
+    ]
+
+}
