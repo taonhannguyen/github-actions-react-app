695df9c0993f602c8f55b70fae3bdf8e638f6c4f
Tao-Nhan Nguyen <tao.nhan@gmail.com>
2021-07-05 14:04:53 -0400

    Core helper methods samples

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,13 +3,23 @@ const github = require('@actions/github'); // npm install @actions/github
 
 try {
     // throw new Error("some error message");
+
+    core.debug('debug log');
+    core.warning('warning log');
+    core.error('error log');
+
     const name = core.getInput('who-to-greet');
+    core.setSecret(name); // makes this variable hidden from log
     console.log(`Hello ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging github object'); // Log that can be expanded in github console
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+
+    core.exportVariable('HELLO', 'hello'); // setup an environment variable accesssible in the workflow step
 }
 catch (error) {
     core.setFailed(error.message);
